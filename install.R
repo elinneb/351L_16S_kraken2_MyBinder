b66ea2837b991424fc91f4436b588ba37cebaea5
Elinne Becket <67041761+elinneb@users.noreply.github.com>
2023-11-07 22:47:33 -0800

    Create install.R

---
+++ install.R
@@ -0,0 +1,10 @@
+install.packages("vegan")
+install.packages("dendextend")
+install.packages("tidyr")
+install.packages("viridis")
+install.packages("reshape")
+
+install.packages("BiocManager")
+
+BiocManager::install("phyloseq")
+BiocManager::install("DESeq2")
